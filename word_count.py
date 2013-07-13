2d9b7ee7e9add958551a40808a5baca3926df6da
Michael O'Sullivan <mifrosu@gmail.com>
2013-07-13 11:52:23 +0100

    functional cli program

--- word_count.py
+++ word_count.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# written by Michael O'Sullivan (mifrosu@gmail.com)
+
 from __future__ import print_function
 from __future__ import with_statement
-from __future__ import unicode_literals
 
+import argparse
 import re
-
-# Q. Write a program, preferably in Python, that given a text file, counts the
-# number of words and reports the top N words ordered by the number of times 
-# they appear in the file.
-
-# As well as seeing an answer, I'd also appreciate a note on how long the 
-# program took to write, and a summary of any research done.
-
-# 11.30
+from operator import itemgetter
 
 class WordCount(object):
 
@@ -20,24 +15,23 @@ class WordCount(object):
         self.file_name = file_name
         self.n = n if n > 0 else -1
         self.word_log = {}
+        self.stats_tuple_list = []
 
     def count_frequency(self, word):
-        '''add to a word frequency dict hash table'''
+        '''reads/creates and increments word_log entry'''
         self.word_log[word] = self.word_log.get(word,0) + 1
 
     def process_line(self, line):
-        temp_list = line.split()
-        # check if words feature a non-alphanumeric char at either end,
-        # e.g. Spanish usage, remove char if present
+        '''removes punctuation (inc. prefixes) and calls count_frequency'''
+        temp_list = line.lower().split()
         check = re.compile("\W")
         for i in range(0,len(temp_list)):
             while check.search(temp_list[i]) != None:
                 k = check.search(temp_list[i])
-                if k and k.start() != 0:
+                if k.start() != 0:
                     temp_list[i] = temp_list[i][:k.start()] 
-                elif k and  k.start() == 0:
+                elif k.start() == 0:
                     temp_list[i] = temp_list[i][1:]
-            print(temp_list[i])
             self.count_frequency(temp_list[i])
 
     def process_file(self):
@@ -46,26 +40,52 @@ class WordCount(object):
                 for line in in_file:
                     self.process_line(line)
         except IOError:
-            print("There was a problem processing the file.")
+            print("There was a problem processing {0}".format(self.file_name))
+            raise
 
-
-
-
-
-
-
-
-
-
-
+    def generate_stats(self):
+        '''Creates a list of tuples from {'key': value}
         
+        The list is sorted alphabetically by 'key', then descending
+        by value.
+        '''
+        self.stats_tuple_list = self.word_log.items()
+        self.stats_tuple_list.sort()
+        self.stats_tuple_list.sort(key=itemgetter(1), reverse=True)
 
-        
+    def print_stats(self, in_tuple_list):
+        max_digits = len(str(in_tuple_list[0][1])) 
+        for i in in_tuple_list:
+            print("{1: >{0}} {2}".format(max_digits+1, i[1], i[0]))
 
+    def print_stats_range(self):
+        if self.n < 1 or self.n > len(self.stats_tuple_list):
+            self.print_stats(self.stats_tuple_list)
+        else:
+            self.print_stats(self.stats_tuple_list[:self.n])
 
+    def run(self):
+        self.process_file()
+        self.generate_stats()
+        self.print_stats_range()
 
 def main():
-    print("This will count files.")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name", 
+                        help="Get entire word frequency list")
+    parser.add_argument("-n", "--number", type=int,
+                        help="Specify top n word frequencies to view. " 
+                        "If n is less than 1 or greater than the number "
+                        "of words in the frequency list, the entire "
+                        "word frequency list is printed.")
+    args = parser.parse_args()
+    if args.file_name and args.number:
+        main_count = WordCount(args.file_name, args.number)
+    elif args.file_name:
+        main_count = WordCount(args.file_name) 
+    if main_count:
+        main_count.run()
+
 
 if __name__ == '__main__':
     main()
--- word_count_test.py
+++ word_count_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# written by Michael O'Sullivan (mifrosu@gmail.com)
+
 from __future__ import print_function
-from __future__ import unicode_literals
 from __future__ import with_statement
 
 import word_count as wc
@@ -16,7 +17,11 @@ class WordCountTest(unittest.TestCase):
 
     def create_test_file(self):
         ''' we will create a test file with some text '''
-        test_text = ("In a hole in the ground there lived a Hobbit"
+        test_text = ("In a hole in the ground there lived a Hobbit. "
+                     "Not a nasty, dirty, wet hole, filled with the ends "
+                     "of worms and an oozy smell, nor yet a dry, bare, "
+                     "sandy hole with nothing in it to sit down on or to"
+                     "eat: it was a hobbit-hole, and that means comfort"
                      ".\n")
         with open(self.file_name, 'wt') as output:
             output.write(test_text)
@@ -35,21 +40,33 @@ class WordCountTest(unittest.TestCase):
         self.assertEqual("a_file.txt", test_count.file_name)
         self.assertEqual(29, test_count.n)
 
-    def test_read_filename(self):
-        '''read the file name into a hash'''
-        self.create_test_file()
-        self.delete_test_file()
-
     def test_count_frequency(self):
         self.test_count.count_frequency("crocodile")
         self.test_count.count_frequency("crocodile")
         self.assertEqual(2, self.test_count.word_log.get("crocodile"))
 
     def test_process_line(self):
-        self.test_count.process_line("!giraffe!!! giraffe! giraffe")
+        '''check for punctuation removal and case change'''
+        self.test_count.process_line("!giraffe!!! giraffe! Giraffe")
         self.assertEqual(3, self.test_count.word_log.get("giraffe"))
-
         
+    def test_process_file(self):
+        self.create_test_file()
+        self.test_count.process_file()
+        self.assertEqual(3, self.test_count.word_log.get("hole"))
+        self.delete_test_file()
+
+    def test_process_file_ioerror(self):
+        '''file does not exist'''
+        print("\nNow testing for IOError: we should see an error msg->")
+        self.assertRaises(IOError, self.test_count.process_file)
+
+    def test_generate_stats(self):
+        self.test_count.process_line("to be or not to be")
+        self.test_count.generate_stats()
+        self.assertEqual([("be", 2), ("to", 2), ("not", 1), ("or", 1)],
+                         self.test_count.stats_tuple_list)
+        self.test_count.print_stats(self.test_count.stats_tuple_list)
 
 
 if __name__ == '__main__':
